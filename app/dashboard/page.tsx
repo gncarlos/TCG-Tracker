0afa9a652ca1f8e5343221c71012d6b8deff5335
Carlos Gonzalez <carlos.gonzalez_arce@us.af.mil>
2024-09-05 21:36:11 -0600

    added basic page and ui file structure

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      DashboardPage
+    </div>
+  );
+}
---
+++ app/landing/page.tsx
@@ -0,0 +1,8 @@
+const LandingPage = () => {
+  return (
+    <div>
+      LandingPage
+
+    </div>
+  );
+}
